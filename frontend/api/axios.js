be85b2b98a3f0593fd7e9444decf7b1b4a346cd2
MayurK-cmd <mayurgk2006@gmail.com>
2026-03-04 11:42:21 +0530

    api fix

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: 
-  //import.meta.env.VITE_BACKEND_URL || 
-  "http://localhost:3000",
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
 });
 
 api.interceptors.request.use((config) => {
